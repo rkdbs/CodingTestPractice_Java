ea9d423f8fb690c9bfb8324285a01e1e303dc7b5
rkdbs <w2207@e-mirim.hs.kr>
2023-03-21 22:38:14 +0900

    중복된 문자 제거

---
+++ src/a0321.java
@@ -0,0 +1,13 @@
+// 중복된 문자 제거
+/*
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+            if(i == my_string.indexOf(my_string.charAt(i)))
+                answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
+ */
