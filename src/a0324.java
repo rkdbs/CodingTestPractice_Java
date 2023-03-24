d971bdac52467635fd157091d4b25f716f158dc2
rkdbs <w2207@e-mirim.hs.kr>
2023-03-24 21:44:20 +0900

    아이스 아메리카노

---
+++ src/a0324.java
@@ -0,0 +1,9 @@
+// 아이스 아메리카노 : https://school.programmers.co.kr/learn/courses/30/lessons/120819
+/*
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {money/5500, money%5500};
+        return answer;
+    }
+}
+*/
