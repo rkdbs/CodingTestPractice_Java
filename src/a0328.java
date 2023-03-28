462d7f5650578bf52c5e85fd2cab232daa65e582
rkdbs <w2207@e-mirim.hs.kr>
2023-03-28 23:29:01 +0900

    숨어있는 숫자의 덧셈(1)

---
+++ src/a0328.java
@@ -0,0 +1,11 @@
+// 숨어있는 숫자의 덧셈(1)
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str[] = my_string.replaceAll("[a-zA-Z]", "").split("");
+        for(int i=0; i<str.length; i++) {
+            answer += Integer.parseInt(str[i]);
+        }
+        return answer;
+    }
+}
