6051fdb0940ff322fd6959ed591ae839ec608cb7
rkdbs <w2207@e-mirim.hs.kr>
2023-03-30 23:30:26 +0900

    피자 나눠 먹기 (3)

---
+++ src/a0330.java
@@ -0,0 +1,11 @@
+// 피자 나눠 먹기 (3)
+/*
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n/slice;
+        if(n%slice!=0)
+            answer += 1;
+        return answer;
+    }
+}
+*/
