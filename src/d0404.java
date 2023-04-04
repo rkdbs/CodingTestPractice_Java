2699d288f2befc053bd3434d0d0847ad21cb2e28
rkdbs <w2207@e-mirim.hs.kr>
2023-04-04 13:38:59 +0900

    각도기

---
+++ src/d0404.java
@@ -0,0 +1,15 @@
+// 각도기
+/*
+class Solution {
+    public int solution(int angle) {
+        int answer = 4;
+        if(angle < 90)
+            answer = 1;
+        else if(angle == 90)
+            answer = 2;
+        else if(angle < 180)
+            answer = 3;
+        return answer;
+    }
+}
+*/
