64ab923bc096645a5c823ad5b1bd1912313b3a01
rkdbs <w2207@e-mirim.hs.kr>
2023-04-05 21:16:59 +0900

    두 수의 나눗셈

---
+++ src/a0405.java
@@ -0,0 +1,9 @@
+// 두 수의 나눗셈
+/*
+class Solution {
+    public int solution(int num1, int num2) {
+        double answer = (double) num1 / num2 * 1000; // double형으로 강제 형변환
+        return (int)answer; // int형으로 강제 형변환
+    }
+}
+*/
