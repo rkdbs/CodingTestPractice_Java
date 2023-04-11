2b108d91af247119da920a1d2325614f9332208e
rkdbs <w2207@e-mirim.hs.kr>
2023-04-11 21:07:44 +0900

    짝수와 홀수

---
+++ src/a0411.java
@@ -0,0 +1,13 @@
+// 짝수와 홀수
+/*
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num%2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
+        return answer;
+    }
+}
+*/
