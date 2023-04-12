04d5908b36cf738b0ea5c3919d9ea0db5783bde6
rkdbs <w2207@e-mirim.hs.kr>
2023-04-12 20:40:57 +0900

    점의 위치 구하기

---
+++ src/a0412.java
@@ -0,0 +1,14 @@
+// 점의 위치 구하기
+/*class Solution {
+public int solution(int[] dot) {
+        int answer = 4;
+        if(dot[0] > 0 && dot[1] > 0)
+            answer = 1;
+        else if(dot[0] < 0 && dot[1] > 0)
+            answer = 2;
+        else if(dot[0] < 0 && dot[1] < 0)
+            answer = 3;
+        return answer;
+    }
+}
+*/
