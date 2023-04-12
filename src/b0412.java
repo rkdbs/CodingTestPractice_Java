25e9ae216f7666e56f8dc6c7aba303c3246dc4f7
rkdbs <w2207@e-mirim.hs.kr>
2023-04-12 20:41:28 +0900

    특정 문자 제거하기

---
+++ src/b0412.java
@@ -0,0 +1,9 @@
+// 특정 문자 제거하기
+/*
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = my_string.replace(String.valueOf(letter), "");
+        return answer;
+    }
+}
+*/
