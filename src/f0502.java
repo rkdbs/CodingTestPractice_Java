1f250aca24f430cc129289be6f479975878f2ec7
rkdbs <w2207@e-mirim.hs.kr>
2023-05-02 21:40:10 +0900

    A 강조하기

---
+++ src/f0502.java
@@ -0,0 +1,10 @@
+// A 강조하기
+public class f0502 {
+	class Solution {
+	    public String solution(String myString) {
+	        myString = myString.toLowerCase();
+	        myString = myString.replace('a', 'A');
+	        return myString;
+	    }
+	}
+}
