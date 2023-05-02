f20f5369f0f1303c575bee8b91179be2771b43c9
rkdbs <w2207@e-mirim.hs.kr>
2023-05-02 21:21:52 +0900

    n의 배수

---
+++ src/c0502.java
@@ -0,0 +1,11 @@
+// n의 배수
+public class c0502 {
+	class Solution {
+	    public int solution(int num, int n) {
+	        int answer = 0;
+	        if(num%n==0)
+	            answer = 1;
+	        return answer;
+	    }
+	}
+}
