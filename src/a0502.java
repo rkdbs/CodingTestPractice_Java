49281ff5ac02ee40d146f5982fa5927e8e857e4f
rkdbs <w2207@e-mirim.hs.kr>
2023-05-02 21:16:25 +0900

    대문자로 바꾸기

---
+++ src/a0502.java
@@ -0,0 +1,9 @@
+// 대문자로 바꾸기
+public class a0502 {
+	class Solution {
+	    public String solution(String myString) {
+	        String answer = myString.toUpperCase();
+	        return answer;
+	    }
+	}
+}
