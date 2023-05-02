1151878d57345cba1bf978031c47ed9b5e79f40b
rkdbs <w2207@e-mirim.hs.kr>
2023-05-02 21:28:10 +0900

    소문자로 바꾸기

---
+++ src/e0502.java
@@ -0,0 +1,9 @@
+// 소문자로 바꾸기
+public class e0502 {
+	class Solution {
+	    public String solution(String myString) {
+	        String answer = myString.toLowerCase();
+	        return answer;
+	    }
+	}
+}
