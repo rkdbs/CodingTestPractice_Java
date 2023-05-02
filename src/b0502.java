5f87d3d4ba4351b86aeda2def602bc1ef6b88fcb
rkdbs <w2207@e-mirim.hs.kr>
2023-05-02 21:19:25 +0900

    flag에 따라 다른 값 반환하기

---
+++ src/b0502.java
@@ -0,0 +1,12 @@
+// flag에 따라 다른 값 반환하기
+public class b0502 {
+	class Solution {
+	    public int solution(int a, int b, boolean flag) {
+	        if(flag == true) {
+	            return a + b;   
+	        }
+	        else
+	            return a - b;
+	    }
+	}
+}
