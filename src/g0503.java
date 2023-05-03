24e3ba4f8a4bdc6f98481f04123bc5e2f1dfb28e
rkdbs <w2207@e-mirim.hs.kr>
2023-05-03 22:39:43 +0900

    문자열을 정수로 변환하기

---
+++ src/g0503.java
@@ -0,0 +1,9 @@
+// 문자열을 정수로 변환하기
+public class g0503 {
+	class Solution {
+	    public int solution(String n_str) {
+	        int answer = Integer.parseInt(n_str);
+	        return answer;
+	    }
+	}
+}
