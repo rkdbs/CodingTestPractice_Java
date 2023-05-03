39180fb58d5f7316fe67d429d59a7d5084a11057
rkdbs <w2207@e-mirim.hs.kr>
2023-05-03 22:16:55 +0900

    홀짝 구분하기

---
+++ src/b0503.java
@@ -0,0 +1,14 @@
+// 홀짝 구분하기
+import java.util.Scanner;
+public class b0503 {
+	public class Solution {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        if(n%2==0)
+	            System.out.print(n + " is even");
+	        else
+	            System.out.print(n + " is odd");
+	    }
+	}
+}
