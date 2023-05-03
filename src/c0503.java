1547832f9b738f75d33a01dfdf8f9437e12c6893
rkdbs <w2207@e-mirim.hs.kr>
2023-05-03 22:18:54 +0900

    공백으로 구분하기 1

---
+++ src/c0503.java
@@ -0,0 +1,9 @@
+// 공백으로 구분하기 1
+public class c0503 {
+	class Solution {
+	    public String[] solution(String my_string) {
+	        String[] answer = my_string.split(" ");
+	        return answer;
+	    }
+	}
+}
