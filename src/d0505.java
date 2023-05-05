88bd41760887711657d18a54f7d84aa0407d2ba3
rkdbs <w2207@e-mirim.hs.kr>
2023-05-05 23:35:36 +0900

    수 조작하기 1

---
+++ src/d0505.java
@@ -0,0 +1,18 @@
+// 수 조작하기 1
+public class d0505 {
+	class Solution {
+	    public int solution(int n, String control) {
+	        for(int i=0; i<control.length(); i++) {
+	            if(control.charAt(i) == 'w')
+	                n += 1;
+	            else if(control.charAt(i) == 's')
+	                n -= 1;
+	            else if(control.charAt(i) == 'd')
+	                n += 10;
+	            else
+	                n -= 10;
+	        }
+	        return n;
+	    }
+	}
+}
