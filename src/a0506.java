aea7447603a6d3b15addcb4a5e1d9c8b055529ec
rkdbs <w2207@e-mirim.hs.kr>
2023-05-06 22:58:19 +0900

    문자열의 앞의 n글자

---
+++ src/a0506.java
@@ -0,0 +1,9 @@
+// 문자열의 앞의 n글자
+public class a0506 {
+	class Solution {
+	    public String solution(String my_string, int n) {
+	        String answer = my_string.substring(0, n);
+	        return answer;
+	    }
+	}
+}
