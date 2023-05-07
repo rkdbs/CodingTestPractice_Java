789ea177b16dab3ce1544cf24bd68704eb46b130
rkdbs <w2207@e-mirim.hs.kr>
2023-05-07 23:24:36 +0900

    문자 리스트를 문자열로 변환하기

---
+++ src/a0507.java
@@ -0,0 +1,9 @@
+// 문자 리스트를 문자열로 변환하기
+public class a0507 {
+	class Solution {
+	    public String solution(String[] arr) {
+	        String answer = String.join("", arr);
+	        return answer;
+	    }
+	}
+}
