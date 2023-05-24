dc7f4dc28d960d153567ba7e8153935dba8bcbfd
rkdbs <w2207@e-mirim.hs.kr>
2023-05-24 10:50:17 +0900

    옷가게 할인 받기

---
+++ src/a0524.java
@@ -0,0 +1,15 @@
+// 옷가게 할인 받기
+public class a0524 {
+	class Solution {
+	    public int solution(int price) {
+	        int answer = price;
+	        if( price >= 500000)
+	            answer = (int)(price*0.8);
+	        else if( price >= 300000)
+	            answer = (int)(price * 0.9);
+	        else if (price >= 100000)
+	            answer = (int)(price * 0.95);
+	        return answer;
+	    }
+	}
+}
